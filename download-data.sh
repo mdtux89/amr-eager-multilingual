41cd32c706dcf344e53e245e9a311d89916b0c88
dammarco <dammarco@amazon.com>
2020-05-29 06:46:15 +0100

    update server

--- download-data.sh
+++ download-data.sh
@@ -1,18 +1,18 @@
 mkdir data
 cd data
 
-wget http://kinloch.inf.ed.ac.uk/public/direct/amreager/europarl-it.tar.gz
+wget http://bollin.inf.ed.ac.uk/public/direct/amreager/europarl-it.tar.gz
 tar -xf europarl-it.tar.gz
 rm -f europarl-it.tar.gz
 
-wget http://kinloch.inf.ed.ac.uk/public/direct/amreager/europarl-es.tar.gz 
+wget http://bollin.inf.ed.ac.uk/public/direct/amreager/europarl-es.tar.gz 
 tar -xf europarl-es.tar.gz
 rm -f europarl-es.tar.gz
 
-wget http://kinloch.inf.ed.ac.uk/public/direct/amreager/europarl-de.tar.gz
+wget http://bollin.inf.ed.ac.uk/public/direct/amreager/europarl-de.tar.gz
 tar -xf europarl-de.tar.gz
 rm -f europarl-de.tar.gz
 
-wget http://kinloch.inf.ed.ac.uk/public/direct/amreager/un-zh.tar.gz 
+wget http://bollin.inf.ed.ac.uk/public/direct/amreager/un-zh.tar.gz 
 tar -xf un-zh.tar.gz
 rm -f un-zh.tar.gz
